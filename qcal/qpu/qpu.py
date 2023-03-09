34be84d892f93fd10c4f8a44948b5c98dff9b38a
Akel Hashim <ahashim@berkeley.edu>
2023-03-08 16:56:54 -0800

    Basic QPU structure

---
+++ qcal/qpu/qpu.py
@@ -0,0 +1,214 @@
+"""Quantum Processing Unit (QPU)
+    
+This submodule is used to run all experiments on hardware.
+
+Basic example useage:
+
+    qpu = QPU(config)
+    qpu.run(circuit)
+"""
+import logging
+import numpy as np
+import pandas as pd
+import timeit
+
+from IPython.display import clear_output
+from typing import Any
+
+from qcal.config import Config
+from qcal.circuits import CircuitSet
+
+logger = logging.getLogger(__name__)
+
+
+class QPU:
+
+    __slots__ = (
+        '_config',
+        '_n_shots',
+        '_n_batches',
+        '_n_circs_per_seq',
+        '_circuits',
+        '_compiled_circuits',
+        '_sequence',
+        '_runtime'
+    )
+
+    def __init__(
+            self,
+            config: Config,
+            n_shots: int = 1024,
+            n_batches: int = 1,
+            n_circs_per_seq: int = 100,
+    ) -> None:
+        
+        self._config = config
+        self._n_shots = n_shots
+        self._n_batches = n_batches
+        self._n_circs_per_seq = n_circs_per_seq
+
+        self._circuits = CircuitSet()
+        self._compiled_circuits = CircuitSet()
+        self._sequence = None
+
+        self._runtime = pd.DataFrame({
+            'Compile':   0.0,
+            'Transpile': 0.0,
+            'Measure':   0.0,
+            'Process':   0.0,
+            'Total':     0.0},
+          index=['Time (s)']
+        )
+
+    @property
+    def circuits(self) -> pd.DataFrame:
+        """Returns a DataFrame view of the circuits
+        
+        Returns:
+            pd.DataFrame: DataFrame of circuits and results for all circuits
+        """
+        return self._circuits._df
+
+    @property
+    def compiled_circuits(self) -> pd.DataFrame:
+        """Returns a DataFrame view of the compiled circuits
+        
+        Returns:
+            pd.DataFrame: DataFrame of circuits and results for all circuits
+        """
+        return self._compiled_circuits._df
+    
+    @property
+    def seq(self):
+        """Returns the current sequence.
+        
+        Returns:
+            qcal.sequencer.sequencer.Sequence: pulse sequence.
+        """
+        return self._sequence
+        
+    def compile(self, circuits) -> None:
+        
+        compiled_circuits = None
+        self._compiled_circuits.append(compiled_circuits)
+        return compiled_circuits
+
+    def transpile(self, circuits=None) -> None:
+
+        if circuits is None:
+            circuits = self._compiled_circuits.Circuits.to_list()
+        
+        # TODO
+
+    def measure(self) -> None:
+        pass
+
+    def process(self) -> None:
+        pass
+
+    def batch(self) -> None:
+
+        n_circ_batches = int(
+            np.ceil(self._circuits.n_circuits / self._n_circs_per_seq)
+        )
+        print((
+            f"Dividing {self._circuits.n_circuits} circuits into "
+            f"{n_circ_batches} batches of size {self._n_circs_per_seq}..."
+        ))
+        for i, circuits in enumerate(self._circuits.batch(
+            self._n_circs_per_seq)):
+            if i > 4:
+                clear_output(wait=True)
+            print(
+                f"Batch {i+1}/{n_circ_batches}: {circuits.n_circuits} circuits"
+            )
+        
+            t0 = timeit.default_timer()
+            compiled_circuits = self.compile(circuits)
+            self.runtime['Compile'][0] += round(timeit.default_timer() - t0, 1)
+
+            t0 = timeit.default_timer()
+            self.transpile(compiled_circuits)
+            self.runtime['Transpile'][0] += round(
+                timeit.default_timer() - t0, 1
+            )
+
+            t0 = timeit.default_timer()
+            self.measure()
+            self.runtime['Measure'][0] += round(timeit.default_timer() - t0, 1)
+
+            t0 = timeit.default_timer()
+            self.process()
+            self.runtime['Process'][0] += round(timeit.default_timer() - t0, 1)
+
+    def save(self) -> None:
+
+        filepath = ''  # TODO
+        
+        self._circuits._df.to_pickle(f'{filepath}circuits.pkl')
+        self._compiled_circuits._df.to_pickle(f'{filepath}compiled_circuits.pkl')
+        self._runtime.to_csv(f'{filepath}runtime.csv')
+
+        print(f"Data save location: {filepath}*\n")
+
+    def run(
+            self,
+            circuits: Any,
+            n_shots=None,
+            n_batches=None,
+            save: bool = True,
+    ) -> None:
+        
+        if not isinstance(circuits, CircuitSet):
+            self._circuits = CircuitSet(circuits=circuits)
+        else:
+            self._circuits = circuits
+
+        if n_shots is not None:
+            self._n_shots = n_shots
+        if n_batches is not None:
+            self._batches = n_batches
+            
+        self._runtime = pd.DataFrame({
+            'Compile':   0.0,
+            'Transpile': 0.0,
+            'Measure':   0.0,
+            'Process':   0.0,
+            'Total':     0.0},
+          index=['Time (s)']
+        )
+        
+        t_start = timeit.default_timer()
+        if self._circuits.n_circuits <= self._n_circs_per_seq:
+            print('No batching...')
+            
+            t0 = timeit.default_timer()
+            self.compile(self._circuits)
+            self.runtime['Compile'][0] += round(timeit.default_timer() - t0, 1)
+
+            t0 = timeit.default_timer()
+            self.transpile()
+            self.runtime['Transpile'][0] += round(
+                timeit.default_timer() - t0, 1
+            )
+
+            t0 = timeit.default_timer()
+            self.measure()
+            self.runtime['Measure'][0] += round(timeit.default_timer() - t0, 1)
+
+            t0 = timeit.default_timer()
+            self.process()
+            self.runtime['Process'][0] += round(timeit.default_timer() - t0, 1)
+
+        else:
+            self.batch()
+
+        self._runtime['Total'][0] += round(timeit.default_timer() - t_start, 1)
+
+        clear_output(wait=True)
+        print("Done!")
+        
+        if save:
+            self.save()
+
+        print(f"Runtime: {repr(self._runtime)[8:]}\n")
