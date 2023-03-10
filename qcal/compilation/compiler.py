61f79f2e45407843a8d6a73f7c99ce3efc774ddc
Akel Hashim <ahashim@berkeley.edu>
2023-03-10 13:05:08 -0800

    Add a default compiler

---
+++ qcal/compilation/compiler.py
@@ -0,0 +1,13 @@
+"""Submodule for constructing a circuit compiler.
+"""
+
+
+class Compiler:
+
+    def __init__(self, passes) -> None:
+        pass
+
+    def compile(self, circuits):
+        pass
+
+DEFAULT_COMPILER = Compiler(None)
