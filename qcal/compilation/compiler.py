981eecc6fcc5e3778e012b167626ff9a36442dc8
Akel Hashim <ahashim@berkeley.edu>
2023-06-27 10:53:23 -0700

    Initial commit for compiler

--- qcal/compilation/compiler.py
+++ qcal/compilation/compiler.py
@@ -1,10 +1,23 @@
 """Submodule for constructing a circuit compiler.
 """
+from qcal.config import Config
 
+import bqskit
+from bqskit import compile
+from bqskit import MachineModel
+from bqskit.ir import Circuit as BqCircuit
+from bqskit.ir.gates import BarrierPlaceholder as BqBarrier
+from bqskit.ir.gates import ConstantUnitaryGate
+from bqskit.ir.gates import (
+    CZGate, XGate, RZGate, SXGate, #ZGate, SGate, SdgGate, TGate, TdgGate
+)
+
+import logging
+logger = logging.getLogger(__name__)
 
 class Compiler:
 
-    def __init__(self, passes) -> None:
+    def __init__(self, config: Config) -> None:
         pass
 
     def compile(self, circuits):
