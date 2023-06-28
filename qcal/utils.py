5b68498bc41beeb6fe788bcb5b15a11939d05fb5
akelhashim <ahashim@berkeley.edu>
2023-06-28 14:08:01 -0700

    Add saving and loading pickle files

--- qcal/utils.py
+++ qcal/utils.py
@@ -1,2 +1,34 @@
 """Helper functions.
 """
+import logging
+import pickle
+
+from typing import Any
+
+logger = logging.getLogger(__name__)
+
+
+def save(data: Any, filename: str) -> None:
+    """Save data to a pickle file.
+
+    Args:
+        data (Any): data of any datatype.
+        filename (str): filename for saved data.
+    """
+    with open(f'{filename}.pkl', 'wb') as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load(filename: str) -> Any:
+    """Load data from a pickle file.
+
+    Args:
+        filename (str): filename of the saved data.
+
+    Returns:
+        Any: loaded data.
+    """
+    with open(f'{filename}.pkl', 'rb') as handle:
+        data = pickle.load(handle)
+    return data
+
