50f11a73151a74ee3bc18a43a648e6d6217d2817
Akel Hashim <ahashim@berkeley.edu>
2024-02-05 13:00:30 -0800

    Add trueq version print statement

--- qcal/benchmarking/rb.py
+++ qcal/benchmarking/rb.py
@@ -119,6 +119,9 @@ def SRB(qpu:             QPU,
             from qcal.interface.trueq.compiler import Compiler
             from qcal.interface.trueq.transpiler import Transpiler
             
+            import trueq as tq
+            print(f"True-Q version: {tq.__version__}\n")
+
             self._qubit_labels = qubit_labels
             self._circuit_depths = circuit_depths
             self._n_circuits = n_circuits
