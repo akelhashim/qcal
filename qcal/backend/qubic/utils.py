5abbd5c8364e2a881a24cbbeabb13881fd6b81fb
Akel Hashim <awsh19@gmail.com>
2025-05-24 22:22:13 -0700

    Add generate_pulse

--- qcal/backend/qubic/utils.py
+++ qcal/backend/qubic/utils.py
@@ -2,16 +2,20 @@
 
 """
 from qcal.config import Config
+from qcal.sequence.pulse_envelopes import pulse_envelopes
 from qcal.units import ns, us
 
 import logging
 import numpy as np
 import pandas as pd
 
+from numpy.typing import NDArray
+from typing import Dict
+
 logger = logging.getLogger(__name__)
 
 
-def calculate_n_reads(config: Config, compiled_program) -> dict:
+def calculate_n_reads(compiled_program) -> dict:
     """Calculate the number of reads per qubit per circuit.
 
     The number of reads will depend on:
@@ -21,7 +25,6 @@ def calculate_n_reads(config: Config, compiled_program) -> dict:
     3) readout at the end of the circuit
 
     Args:
-        config (Config): qcal config object.
         compiled_program (distproc.compiler.CompiledProgram): QubiC compiled
             program object.
 
@@ -36,6 +39,41 @@ def calculate_n_reads(config: Config, compiled_program) -> dict:
         n_reads[f'{q}.rdlo'] = sum(seq[q] == 'Readout')
 
     return n_reads
+
+
+def generate_pulse(
+        config: Config, pulse: Dict, include_amp_phase: bool = True    
+    ) -> NDArray[np.complex64]:
+    """Generate a pulse in a form compatible with QubiC.
+
+    Args:
+        config (Config): qcal Config object.
+        pulse (Dict): dictionary object of a pulse returned from indexing a
+            config file.
+        include_amp_phase (bool): whether to include the pulse amp and phase in
+            the generation of the envelope. Defaults to True. By default, QubiC
+            uses False because the amp and phase are dynamically included in
+            compilation. This lowers the waveform memory requirements when
+            generating sequences.
+
+    Returns:
+        NDArray[np.complex64]: complex pulse array.
+    """
+    if not include_amp_phase:
+        kwargs = {}
+        for key, val in pulse['kwargs'].items():
+            if key not in ['amp', 'phase']:
+                kwargs[key] = val
+    else:
+        kwargs = {key: val for key, val in pulse['kwargs'].items()}
+
+    pulse = pulse_envelopes[pulse['env']](
+        pulse['length'],
+        config['hardware/sample_rate/DAC'] / 
+            config['hardware/interpolation_ratio/qdrv'],
+        **kwargs
+    )
+    return pulse
 
 
 def qubic_sequence(compiled_program) -> pd.DataFrame:
