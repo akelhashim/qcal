9edff697a8c70cd4208456fd5fb96106e1d92f19
Akel Hashim <awsh19@gmail.com>
2025-09-18 10:16:22 -0700

    Add styler for displaying confusion matrix

--- qcal/benchmarking/readout.py
+++ qcal/benchmarking/readout.py
@@ -68,12 +68,23 @@ def ReadoutFidelity(
             self._qubits = sorted(qubits)
             assert gate in ('X90', 'X'), 'gate must be an X90 or X!'
             self._gate = gate
-            self._confusion_mat = None
+            self._cmat = None
+
+        @property
+        def cmat(self):
+            """Confusion matrix for each qubit."""
+            return self._cmat
 
         @property
         def confusion_matrix(self):
-            """Confusion matrix for each qubit."""
-            return self._confusion_mat
+            """Styled confusion matrix for each qubit."""
+            return self._cmat.astype(float).style.background_gradient(
+                cmap='Blues', vmin=0, vmax=1
+            ).format("{:.3f}").set_properties(**{
+                'font-size': '12pt',
+                'padding': '8px',
+                'text-align': 'center'
+            })
 
         def generate_circuits(self):
             """Generate the readout calibration circuits."""
@@ -132,13 +143,13 @@ def ReadoutFidelity(
                 columns[1].extend(['Meas State'] * self._n_levels)
                 columns[2].extend([n for n in range(self._n_levels)])
 
-            self._confusion_mat = pd.DataFrame(
+            self._cmat = pd.DataFrame(
                 columns=columns, index=index
             )
 
             for i, circuit in enumerate(self._circuits):  # i = prep state
                 for j, q in enumerate(self._qubits):  # j, q = idx, qubit
-                    self._confusion_mat.loc[
+                    self._cmat.loc[
                         ('Prep State', i), (f'Q{q}', 'Meas State')
                     ] = ([
                         circuit.results.marginalize(j).populations[f'{n}']
@@ -154,7 +165,7 @@ def ReadoutFidelity(
             if settings.Settings.save_data:
                 qpu.save(self)
                 self._data_manager.save_to_csv(
-                    self._confusion_mat, 'confusion_matrix'
+                    self._cmat, 'confusion_matrix'
                 )
 
         def final(self):
